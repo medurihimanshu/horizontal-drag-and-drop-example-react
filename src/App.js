320103f84045375dc3bb627216ba422581b3a5de
medurih <medurih@amazon.com>
2022-12-20 20:01:40 +0530

    UI done till sub flow

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   //   // { id: "5", label: "Dustin" },
   // ];
 
+
   useEffect(() => {
     async function fetchData() {
       console.log('start');
@@ -26,7 +27,7 @@ function App() {
     fetchData();
   });
   const original = [
-    { id: "1", label: "Screening" },
+    { id: "1", label: "Screening", metadata: [] },
     { id: "2", label: "Identity" },
     { id: "3", label: "Relationship" }
     // { id: "4", label: "Mike" },
@@ -40,82 +41,13 @@ function App() {
   const [row1List, setRow1List] = useState(row1);
   const [row2List, setRow2List] = useState(row2);
   const [row3List, setRow3List] = useState(row3);
-
+  const [popUpState, setPopUpState] = useState(false);
 
 
 
   
 
-  // const onDragCompleteOrigin = (result) => {
-  //   if (!result.destination) return;
-
-  //   const arr = [...sourceList];
-
-  //   //Changing the position of Array element
-  //   let removedItem = arr.splice(result.source.index, 1)[0];
-  //   arr.splice(result.destination.index, 0, removedItem);
-
-  //   //Updating the list
-  //   setSourceList(arr);
-  // };
-  // const onDragCompleteRow1 = (result) => {
-  //   if (!result.destination) return;
-
-  //   const arr = [...row1List];
-
-  //   //Changing the position of Array element
-  //   let removedItem = arr.splice(result.source.index, 1)[0];
-  //   arr.splice(result.destination.index, 0, removedItem);
-
-  //   //Updating the list
-  //   setRow1List(arr);
-  // };
-  // const onDragCompleteRow2 = (result) => {
-  //   if (!result.destination) return;
-
-  //   const arr = [...row2List];
-
-  //   //Changing the position of Array element
-  //   let removedItem = arr.splice(result.source.index, 1)[0];
-  //   arr.splice(result.destination.index, 0, removedItem);
-
-  //   //Updating the list
-  //   setRow2List(arr);
-  // };
-  // const onDragCompleteRow3 = (result) => {
-  //   // if (!result.destination) return;
-
-  //   const arr = [...row3List];
-
-  //   //Changing the position of Array element
-  //   let removedItem = arr.splice(result.source.index, 1)[0];
-  //   arr.splice(result.destination.index, 0, removedItem);
-
-  //   //Updating the list
-  //   setRow3List(arr);
-  // };
-
-  // const onDropOrigin = (result) => {
-  //   // let id = ev.s.getData("id");
-  //   const arr = [...sourceList];
-  //   let removedItem = result.dataTransfer.getData("item");
-  //   arr.splice(result.destination.index, 0, removedItem);
-  //   setSourceList(arr);
-  // }
-
-  // const onDropRow1 = (result) => {
-  //   // let id = ev.s.getData("id");
-  //   const arr = [...row1List];
-  //   let removedItem = result.dataTransfer.getData("item");
-  //   arr.splice(result.destination.index, 0, removedItem);
-  //   setRow1List(arr);
-  // }
-
-  // const onDragStartRow1 = (ev, item) => {
-  //   // let id = ev.s.getData("id");
   
-  //   ev.dataTransfer.setData("item", item);
-  // }
   const onDragOver = (ev) => {
     ev.preventDefault();
   }
@@ -145,7 +77,7 @@ function App() {
       setRow3List(row3List);
     } else if (source.droppableId === 'source') {
       item = sourceList.splice(source.index,1)[0];
-      setRow1List(sourceList);
+      setSourceList(sourceList);
     }
 
 
@@ -156,7 +88,7 @@ function App() {
       row2List.splice(destination.index,0,item);
       setRow2List(row2List);
     } 
-     if (destination.droppableId === 'row3') {
+    if (destination.droppableId === 'row3') {
       row3List.splice(destination.index,0,item);
       setRow3List(row3List);
     } 
@@ -168,181 +100,202 @@ function App() {
     console.log("Row2 - ", row2List);
     console.log("Row3 - ", row3List);
     console.log("Source - ", sourceList);
- 
+
   }
 
- 
+
 
 
   return (
 
     
     <div className="container">
-      <div className="header"><center>MATAVERI FLOW CONFIGURATION</center></div>
-      <br/>
-      <div className="card">
-        
-
-        <DragDropContext onDragEnd={onDragEnd} onDragOver={onDragOver}>
-          <Droppable droppableId="source" direction="vertical">
-            
-            {(provided, snapshot) => (
-              <div
-                className="drag-drop-list-container-origin"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {sourceList.map((item, index) => (
-                  <Draggable
-                    key={item.id}
-                    draggableId={item.label}
-                    index={index}
-                    // ondrop={onDropOrigin}
-                  >
-                    {(provided) => (
-                      <div
-                        className="item-card"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <span className="material-symbols-outlined">
-                          drag_indicator
-                        </span>
-                        <div className="char-avatar">
-                          {item.label.charAt(0)}
-                        </div>
-                        <p className="label">{item.label}</p>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-          <div className="maves-flow-container">
-            <div className="basic-node">
-              <center>START</center>
-            </div>
-            <Droppable droppableId="row1" direction="horizontal" >
-              {(provided, snapshot) => (
-                <div
-                  className="drag-drop-list-container"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {row1List.map((item, index) => (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.label}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <div
-                          className="item-card"
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <span className="material-symbols-outlined">
-                            drag_indicator
-                          </span>
-                          <div className="char-avatar">
-                            {item.label.charAt(0)}
-                          </div>
-                          <p className="label">{item.label}</p>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-
-            <Droppable droppableId="row2" direction="horizontal" >
-              {(provided, snapshot) => (
-                <div
-                  className="drag-drop-list-container"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {row2List.map((item, index) => (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.label}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <div
-                          className="item-card"
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <span className="material-symbols-outlined">
-                            drag_indicator
-                          </span>
-                          <div className="char-avatar">
-                            {item.label.charAt(0)}
-                          </div>
-                          <p className="label">{item.label}</p>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-
-            <Droppable droppableId="row3" direction="horizontal" >
-              {(provided, snapshot) => (
-                <div
-                  className="drag-drop-list-container"
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                >
-                  {row3List.map((item, index) => (
-                    <Draggable
-                      key={item.id}
-                      draggableId={item.label}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <div
-                          className="item-card"
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <span className="material-symbols-outlined">
-                            drag_indicator
-                          </span>
-                          <div className="char-avatar">
-                            {item.label.charAt(0)}
-                          </div>
-                          <p className="label">{item.label}</p>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-
-            <div className="basic-node">
-              <center>STOP</center>
-            </div>
-          </div>
+      <div className="header"><center><h1>MATAVERI  FLOW  CONFIGURATION</h1></center></div>
+        <br/>
+        <div className="card">
           
-        </DragDropContext>
 
+          <DragDropContext onDragEnd={onDragEnd} onDragOver={onDragOver}>
+            <Droppable droppableId="source" direction="vertical">
+              {/* <div className="basic-node">
+                <center><b>END</b></center>
+              </div> */}
+              {(provided, snapshot) => (
+                <div
+                  className="drag-drop-list-container-origin"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {sourceList.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.label}
+                      index={index}
+                      // ondrop={onDropOrigin}
+                    >
+                      {(provided) => (
+                        <div
+                          className="item-card"
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <span className="material-symbols-outlined">
+                            drag_indicator
+                          </span>
+                          <div className="char-avatar">
+                            {item.label.charAt(0)}
+                          </div>
+                          
+                          <h3 className="label"><h3>{item.label}</h3></h3>
+                          
+                          
+                          <button>Configure</button>
+                          
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+            <div className="maves-flow-container">
+              <div className="basic-node">
+                <center><h3>START</h3></center>
+              </div>
+              <br/>
+              <br/>
+              <br/>
+              <Droppable droppableId="row1" direction="horizontal" >
+                {(provided, snapshot) => (
+                  <div
+                    className="drag-drop-list-container"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {row1List.map((item, index) => (
+                      <Draggable
+                        key={item.id}
+                        draggableId={item.label}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            className="item-card"
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <span className="material-symbols-outlined">
+                              drag_indicator
+                            </span>
+                            <div className="char-avatar">
+                              {item.label.charAt(0)}
+                            </div>
+                            <h3 className="label"><h3>{item.label}</h3></h3>
+                            <button>Configure</button>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+              <br/>
+              <br/>  
+              <br/>    
+              <Droppable droppableId="row2" direction="horizontal" >
+                {(provided, snapshot) => (
+                  <div
+                    className="drag-drop-list-container"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {row2List.map((item, index) => (
+                      <Draggable
+                        key={item.id}
+                        draggableId={item.label}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            className="item-card"
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <span className="material-symbols-outlined">
+                              drag_indicator
+                            </span>
+                            <div className="char-avatar">
+                              {item.label.charAt(0)}
+                            </div>
+                            <h3 className="label"><h3>{item.label}</h3></h3>
+                            <button>Configure</button>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+              <br/>
+              <br/>
+              <br/>
+              <Droppable droppableId="row3" direction="horizontal" >
+                {(provided, snapshot) => (
+                  <div
+                    className="drag-drop-list-container"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {row3List.map((item, index) => (
+                      <Draggable
+                        key={item.id}
+                        draggableId={item.label}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            className="item-card"
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <span className="material-symbols-outlined">
+                              drag_indicator
+                            </span>
+                            <div className="char-avatar">
+                              {item.label.charAt(0)}
+                            </div>
+                            <h3 className="label"><h3>{item.label}</h3></h3>
+                            <button>Configure</button>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+              <br/>
+              <br/>
+              <br/>
+              <div className="basic-node">
+                <center><h3>END</h3></center>
+              </div>
+            </div>
+            
+          </DragDropContext>
+          <button>CHECK</button>
 
+          
         
-      </div>
+      
+        </div>
         
 
         
@@ -351,6 +304,9 @@ function App() {
 
     
   );
+
+
+  
 }
 
 export default App;
